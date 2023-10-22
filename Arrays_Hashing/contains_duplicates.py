039916e2edcd7e726a08da43a556acc2ee1b1344
diwahar-sekar <diwaharsekar@gmail.com>
2023-10-22 19:33:13 +0530

    groupAnagrams

--- Arrays_Hashing/contains_duplicates.py
+++ Arrays_Hashing/contains_duplicates.py
@@ -1,5 +1,6 @@
 
 from typing import List
+import collections
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         """
@@ -38,8 +39,22 @@ class Solution:
             hash_map[num] = index
         return []
 
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        """
+        https://leetcode.com/problems/group-anagrams/
+        """
+        result = collections.defaultdict(list)
+
+        for word in strs:
+            s = [0] * 26
+            for ch in word:
+                s[ord(ch) - ord("a")] += 1
+            result[tuple(s)].append(word)
+        return result.values()
 if __name__ == "__main__":
     solution = Solution()
     # print(solution.containsDuplicate([1,2,3,4]))
     # print(solution.isAnagram(s = "anagram", t = "nagaram"))
     # print(solution.twoSum(nums = [2,7,11,15], target = 9))
+    # print(solution.groupAnagrams(strs = ["eat","tea","tan","ate","nat","bat"]))
+#
