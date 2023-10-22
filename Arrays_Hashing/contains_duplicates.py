c0fc83404ed373c43c8e46c2655f8812825a4cc1
diwahar-sekar <diwaharsekar@gmail.com>
2023-10-22 19:14:39 +0530

    Anagram solution

--- Arrays_Hashing/contains_duplicates.py
+++ Arrays_Hashing/contains_duplicates.py
@@ -25,8 +25,21 @@ class Solution:
                 return False
         return True
 
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+
+        https://leetcode.com/problems/two-sum/
+        """
+        hash_map = dict()
+        for index, num in enumerate(nums):
+            difference = target - num
+            if difference in hash_map:
+                return [index, hash_map[difference]]
+            hash_map[num] = index
+        return []
 
 if __name__ == "__main__":
     solution = Solution()
     # print(solution.containsDuplicate([1,2,3,4]))
     # print(solution.isAnagram(s = "anagram", t = "nagaram"))
+    # print(solution.twoSum(nums = [2,7,11,15], target = 9))
