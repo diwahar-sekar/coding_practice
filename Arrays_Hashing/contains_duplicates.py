b70bc938e987369dd41b0a8b33265d7d4fb88cb5
diwahar-sekar <diwaharsekar@gmail.com>
2023-10-22 19:07:39 +0530

    Anagram solution

--- Arrays_Hashing/contains_duplicates.py
+++ Arrays_Hashing/contains_duplicates.py
@@ -1,9 +1,10 @@
-"""
-https://leetcode.com/problems/contains-duplicate/
-"""
+
 from typing import List
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        """
+        https://leetcode.com/problems/contains-duplicate/
+        """
         check = dict()
         for num in nums:
             if num in check:
@@ -11,8 +12,21 @@ class Solution:
             check[num] = 1
 
         return False
-solution = Solution()
+
+    def isAnagram(self, s: str, t: str) -> bool:
+        """
+        https://leetcode.com/problems/valid-anagram/
+        """
+        if len(s) != len(t):
+            return False
+        alphabets = "abcdefghijklmnopqrstuvwxyz"
+        for letters in alphabets:
+            if s.count(letters) != t.count(letters):
+                return False
+        return True
+
 
 if __name__ == "__main__":
-    pass
+    solution = Solution()
     # print(solution.containsDuplicate([1,2,3,4]))
+    # print(solution.isAnagram(s = "anagram", t = "nagaram"))
