3d91671836d9d9a0f263f2d458658b2ebfc974f7
diwahar-sekar <diwaharsekar@gmail.com>
2023-10-22 19:56:18 +0530

    productExceptSelf

--- Arrays_Hashing/contains_duplicates.py
+++ Arrays_Hashing/contains_duplicates.py
@@ -73,6 +73,22 @@ class Solution:
                 if len(result) == k:
                     return result
 
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        """
+        https://leetcode.com/problems/product-of-array-except-self/
+        """
+        prefix, postfix = 1, 1
+        list1 = [1] * (len(nums))
+        for i in range(len(nums)):
+            list1[i]=prefix
+            prefix*=nums[i]
+
+        for j in range(len(nums)-1,-1,-1):
+            list1[j]*=postfix
+            postfix*=nums[j]
+
+        return list1
+
 if __name__ == "__main__":
     solution = Solution()
     # print(solution.containsDuplicate([1,2,3,4]))
@@ -80,3 +96,4 @@ if __name__ == "__main__":
     # print(solution.twoSum(nums = [2,7,11,15], target = 9))
     # print(solution.groupAnagrams(strs = ["eat","tea","tan","ate","nat","bat"]))
     # print((solution.topKFrequent(nums = [1,1,1,2,2,3], k = 2)))
+    # print(solution.productExceptSelf([1,2,3,4]))
